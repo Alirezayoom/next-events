7c980476720c956ff79e160da1396c92c2a11af1
alirezayoom <alirezayoom@gmail.com>
2022-09-21 11:20:35 +0430

    event search added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "./events-search";
 
 const Events = () => {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventsSearch />
       <EventList items={events} />
     </div>
   );
