b077666181a5ab6394f5239c9029aecebc416001
alirezayoom <alirezayoom@gmail.com>
2022-09-21 10:48:23 +0430

    events added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 const Events = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>all events</h1>
+      <EventList items={events} />
     </div>
   );
 };
