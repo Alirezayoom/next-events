f532f8cef5a5338185012131404f3b648159bdfd
alirezayoom <alirezayoom@gmail.com>
2022-09-21 09:00:21 +0430

    event detail added

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,13 +1,32 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react/cjs/react.production.min";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventId = () => {
   const router = useRouter();
   console.log(router.query);
 
+  const eventId = router.query.eventid;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>no event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>event details</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
   );
 };
 
