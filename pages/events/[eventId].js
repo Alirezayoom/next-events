89160ea454cd26c5d5144b80708d65840fede236
alirezayoom <alirezayoom@gmail.com>
2022-12-06 10:50:13 +0430

    event detail added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,33 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
-  return <div>event detail</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailPage;
