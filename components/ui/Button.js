da0ffa585238bbd2d67ceeaae10cfc682640be25
alirezayoom <alirezayoom@gmail.com>
2022-12-08 10:56:24 +0430

    filter form added

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -3,10 +3,18 @@ import Link from "next/link";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
+  if (props.link) {
+    return (
+      <Link className={classes.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+  }
+
   return (
-    <Link className={classes.btn} href={props.link}>
+    <button onClick={props.onClick} className={classes.btn}>
       {props.children}
-    </Link>
+    </button>
   );
 };
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEvents = () => {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventsSearch />
       <EventList items={events} />
     </div>
   );
