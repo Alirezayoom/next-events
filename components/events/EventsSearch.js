35c2af9f938dd5c16ed67409324a2e9f54a69f18
alirezayoom <alirezayoom@gmail.com>
2022-12-10 09:17:49 +0430

    useRef added

--- components/events/EventsSearch.js
+++ components/events/EventsSearch.js
@@ -1,13 +1,27 @@
+import { useRef } from "react";
+
 import Button from "../ui/Button";
 import classes from "./events-search.module.css";
 
 const EventsSearch = (props) => {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function sumbitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sumbitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
@@ -15,7 +29,7 @@ const EventsSearch = (props) => {
 
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEvents = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
