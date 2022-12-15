5b943f2027f7ee0e60068a80aabf2628be471bac
alirezayoom <alirezayoom@gmail.com>
2022-12-15 19:11:11 +0430

    fetch data from firebase

---
+++ helpers/api-u.js
@@ -0,0 +1,23 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://next-events-6cc09-default-rtdb.firebaseio.com/events.json"
+  );
+
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-u";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
+const HomePage = (props) => {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.featuredEvents} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents: featuredEvents,
+    },
+  };
+}
 
 export default HomePage;
