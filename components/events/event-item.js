e4c62734e3930257a3fd932ed4240e4e54621dec
alirezayoom <alirezayoom@gmail.com>
2022-12-17 10:30:44 +0430

    Image added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
 
@@ -19,7 +20,7 @@ const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={240} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
