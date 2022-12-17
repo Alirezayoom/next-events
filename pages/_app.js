a1b620d107d92ca0d12797708fd1cfb5b7b6f1af
alirezayoom <alirezayoom@gmail.com>
2022-12-17 09:51:24 +0430

    Head added

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta
-          name="viewport"
-          description="initial-scale=1.0, width=device-width"
-        />
+        <title>Next Events</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="NextJS Events" />
       </Head>
       <Component {...pageProps} />;
     </Layout>
