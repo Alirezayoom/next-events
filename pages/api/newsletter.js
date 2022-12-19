43da5e5b09160b9fcfd1c8b9505c5f2c35aec287
alirezayoom <alirezayoom@gmail.com>
2022-12-19 19:10:48 +0430

    error handling added

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,18 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ali:ali@cluster0.qafjedp.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const { email } = req.body;
@@ -9,13 +22,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://ali:ali@cluster0.qafjedp.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email });
-    client.close();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "connecting to db faild!" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "connecting to db faild!" });
+      return;
+    }
 
     res.status(201).json({ message: "signed up!" });
   }
